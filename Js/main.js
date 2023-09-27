963406d4790cf40a8932ebc520a37614207adc2c
jonibek0979 <omadbekxoshimov20060715@gmail.com>
2023-09-27 12:55:51 +0500

    inital commit

---
+++ Js/main.js
@@ -0,0 +1,23 @@
+alert("Xush kelibsiz");
+alert("Yoshinggizni kiriting");
+var a = prompt(18)
+if( a > 18 ){
+    console.log("Aka");
+}
+else if( a < 18 ){
+    console.log("Uka");
+}
+else if( a == 18){
+    console.log("Do'st");
+}
+
+
+
+
+
+
+
+
+
+
+
